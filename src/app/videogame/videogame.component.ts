79c6400f8c1db220d5ba894241f4a675116e4164
Jonathan IMG <join_mg@hotmail.com>
2018-09-11 18:41:17 -0500

    Hooks

--- src/app/videogame/videogame.component.ts
+++ src/app/videogame/videogame.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'videogame',
     templateUrl: './videogame.component.html'
 })
 
-export class VideogameComponent{
+export class VideogameComponent implements OnInit, DoCheck, OnDestroy{
     public title: string;
     public list: string;
 
@@ -14,4 +14,21 @@ export class VideogameComponent{
         this.list = 'Most popular videogames'
         console.log('Your component videogame.component is loaded');
     }
+
+    ngOnInit(){
+        console.log('OnInit exeuted');
+    }
+
+    ngDoCheck(){
+        console.log('DoCheck executed');
+    }
+
+    ngOnDestroy(){
+        console.log('OnDestroy executed');   
+    }
+
+    SwitchTitle(){
+        this.title = 'New component title';
+    }
+
 }
