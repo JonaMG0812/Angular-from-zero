bf6c8eda5abaff4f621031eaff7e25f8955937fa
Jonathan IMG <join_mg@hotmail.com>
2018-09-10 19:54:05 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-from-zero';
+  public title = 'Angular-from-zero';
 }
