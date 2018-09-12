7484b6cff8743cf5e95008901e2612ec364b6cac
Jonathan IMG <join_mg@hotmail.com>
2018-09-11 20:51:43 -0500

    Class and Models

--- src/app/shoes/shoes.component.ts
+++ src/app/shoes/shoes.component.ts
@@ -1,13 +1,27 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import { Shoe } from '../Models/shoe';
 
 @Component({
     selector: 'shoes',
     templateUrl: './shoes.component.html'
 })
 
-export class ShoesComponent {
+export class ShoesComponent implements OnInit{
     public ShoesTitle: string = 'Shoes Component';
     public ShoesList: string = 'Most popular shoes';
+    public Shoes: Array<Shoe>;
+
+    constructor(){
+        this.Shoes = [
+            new Shoe('AirMG0812', 'Nike', 2500, 'Red', true),
+            new Shoe('Runner', 'Adidas', 1500, 'White', true),
+            new Shoe('Air Confort', 'New Balance', 2200, 'Blue', false)
+        ];
+    }
+
+    ngOnInit(){
+        console.log(this.Shoes);
+    }
 
     /*
     constructor(){
@@ -16,5 +30,7 @@ export class ShoesComponent {
         console.log('Your component shoes.component is loaded');
     }
     */
+
+    
 
 }
