ce514e4a23b1b255d990a0e8151b7926c5e18264
Jonathan IMG <join_mg@hotmail.com>
2018-09-12 17:24:27 -0500

    ngIf, ngFor

--- src/app/shoes/shoes.component.ts
+++ src/app/shoes/shoes.component.ts
@@ -10,17 +10,27 @@ export class ShoesComponent implements OnInit{
     public ShoesTitle: string = 'Shoes Component';
     public ShoesList: string = 'Most popular shoes';
     public Shoes: Array<Shoe>;
+    public ShoeBrands: string[];
 
     constructor(){
         this.Shoes = [
             new Shoe('AirMG0812', 'Nike', 2500, 'Red', true),
             new Shoe('Runner', 'Adidas', 1500, 'White', true),
-            new Shoe('Air Confort', 'New Balance', 2200, 'Blue', false)
+            new Shoe('Air Confort', 'New Balance', 2200, 'Blue', false),
+            new Shoe('Converse', 'Converse', 1800, 'Black', true),
+            new Shoe('Vanta Black', 'Vanta', 1600, 'Black', false),
+            new Shoe('Air Max', 'Nike', 1200, 'Brown', false)
         ];
     }
 
     ngOnInit(){
         console.log(this.Shoes);
+    }
+
+    ShoeBrands(){
+        this.Shoes.forEach((shoe, index) => {
+            ShoeBrands.push(shoe.brand);
+        });
     }
 
     /*
