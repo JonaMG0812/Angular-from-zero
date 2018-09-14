eba85ba921931cf4c0186ea52ef0963a9fd8aa44
Jonathan IMG <join_mg@hotmail.com>
2018-09-13 21:08:22 -0500

    Config Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing'
 
 import { AppComponent } from './app.component';
 import { VideogameComponent } from './videogame/videogame.component';
 import { ShoesComponent} from './shoes/shoes.component';
 import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideogameComponent,
     ShoesComponent,
-    CoursesComponent
+    CoursesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+//Importing angular routing modules
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//Importing components
+import { HomeComponent } from './home/home.component'
+import { ShoesComponent } from './shoes/shoes.component';
+import { VideogameComponent } from './videogame/videogame.component';
+import { CoursesComponent } from './courses/courses.component';
+
+//Config routes array
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'shoes', component: ShoesComponent},
+    {path: 'videogames', component: VideogameComponent},
+    {path: 'courses', component: CoursesComponent},
+    {path: '**', component: HomeComponent}
+];
+
+//Exporting router module
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
